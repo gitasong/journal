11fdbf6067cd5d1a8fcee012c27d192743288ef9
Nicole Freed and Nathan Stewart <nicole@>
2017-07-31 16:25:39 -0700

    add prototype methods for countVowels() and countConsonants() (not working)

--- js/journal.js
+++ js/journal.js
@@ -7,4 +7,26 @@ Entry.prototype.getWordNumber = function(body) {
   return wordArrayLen;
 };
 
+Entry.prototype.countVowels = function(body) {
+  vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  count = 0;
+  for (var i = 0; i < body.length; i++) {
+    if vowels.includes(body[i]) {
+      count ++;
+    };
+    return count;
+  };
+};
+
+Entry.prototype.countConsonants = function(body) {
+  vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  count = 0;
+  for (var i = 0; i < body.length; i++) {
+    if !(vowels.includes(body[i])) {
+      count ++;
+    };
+    return count;
+  };
+};
+
 exports.entryModule = Entry;
