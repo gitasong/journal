d90f67824bafd926a8e35f51b52034e4c876d7e1
Nicole Freed and Nathan Stewart <nicole@>
2017-07-31 15:53:39 -0700

    move event.preventDefault to submit() function

--- js/journal-interface.js
+++ js/journal-interface.js
@@ -1,8 +1,8 @@
 var Entry = require('./../js/journal.js').entryModule;
 
-$(document).ready(function(event) {
-  event.preventDefault();
-  $("#journal-form").submit(function() {
+$(document).ready(function() {
+  $("#journal-form").submit(function(event) {
+    event.preventDefault();
     var title = $("#title").val();
     var body = $("#body").val();
     var newEntry = new Entry();
