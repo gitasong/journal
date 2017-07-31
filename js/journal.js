78659573137d28c02a20a12bcfc4937f1de1de14
Nicole Freed and Nathan Stewart <nicole@>
2017-07-31 15:54:14 -0700

    remove console.logs

--- js/journal.js
+++ js/journal.js
@@ -3,9 +3,7 @@ function Entry() {
 
 Entry.prototype.getWordNumber = function(body) {
   var wordArray = body.split(" ");
-  console.log(wordArray);
   wordArrayLen = wordArray.length;
-  console.log(wordArrayLen);
   return wordArrayLen;
 };
 
